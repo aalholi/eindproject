c30553c14260045737f804bea3c327d86b078b85
Zeine <zeynedinzeydan552@gmail.com>
2024-06-05 00:13:02 +0200

    update

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -25,32 +24,32 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
 
+    <!-- Flickery CDN -->
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
     <!-- Style sheet -->
     <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/home.css">
     <title>Brawnies Escape Rooms</title>
   </head>
   <body>
-  <header class="navbar d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+        <header class="navbar d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
           <div class="col-md-3 mb-2 mb-md-0">
-            <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <a href="index.html" class="d-inline-flex link-body-emphasis text-decoration-none">
               <img class="bi" width="100" height="60" role="img" aria-label="Bootstrap" src="images/logo.webp"/>
             </a>
           </div>
    
           <ul class="nav nav-links col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="index.php" class="nav-link px-2"><i class="uil uil-estate"></i>Home</a></li>
-            <li><a href="pages/games.php" class="nav-link px-2"><i class="uil uil-club"></i>Games</a></li>
+            <li><a href="index.html" class="nav-link px-2"><i class="uil uil-estate"></i>Home</a></li>
+            <li><a href="games.html" class="nav-link px-2"><i class="uil uil-club"></i>Games</a></li>
             <li><a href="pages/leaderboard.php" class="nav-link px-2"><i class="uil uil-game-structure"></i>Leaderboard</a></li>
-            <li><a href="pages/reserve.php" class="nav-link px-2"><i class="uil uil-schedule"></i>Reservation</a></li>
-            <li><a href="pages/AboutUs.php" class="nav-link px-2"><i class="uil uil-info-circle"></i>About Us</a></li>
+            <li><a href="pages/reservation.php" class="nav-link px-2"><i class="uil uil-schedule"></i>Reservation</a></li>
+            <li><a href="pages/aboutUs.php" class="nav-link px-2"><i class="uil uil-info-circle"></i>About Us</a></li>
           </ul>
           <div class="col-md-3 text-end">
-            <?php if (isset($_SESSION['voornaam'])){?>
-              <button type="button" class="btn loginBtnDesktop btn-dark me-2"><a href="pages/logout.php" class="link-login-button text-decoration-none"><i class="uil uil-signout"></i>Logout</a></button>
-              
-              <?php } else { ?>
-            <button type="button" class="btn loginBtnDesktop btn-dark me-2"><a href="pages/login.php" class="link-login-button text-decoration-none"><i class="uil uil-user"></i>Login</a></button>
-            <?php } ?>
+            <button type="button" class="btn loginBtnDesktop btn-dark me-2"><a href="" class="link-login-button text-decoration-none"><i class="uil uil-user"></i>Login</a></button>
             <nav class="navbar navbar-menu-mobile navbar-dark">
               <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,124 +60,87 @@
             <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
               <div class="navbar-mobile-container bg-dark p-4">
                 <ul class="nav navbar-mobile justify-content-center">
-                <li><a href="../index.php" class="nav-link px-2"><i class="uil uil-estate"></i>Home</a></li>
-            <li><a href="../games.html" class="nav-link px-2"><i class="uil uil-club"></i>Games</a></li>
-            <li><a href="./leaderboard.php" class="nav-link px-2"><i class="uil uil-game-structure"></i>Leaderboard</a></li>
-            <li><a href="./reservation.php" class="nav-link px-2"><i class="uil uil-schedule"></i>Reservation</a></li>
-            <li><a href="./aboutUs.php" class="nav-link px-2"><i class="uil uil-info-circle"></i>About Us</a></li>
+                  <li><a href="index.html" class="nav-link px-2"><i class="uil uil-estate"></i>Home</a></li>
+                  <li><a href="games.html" class="nav-link px-2"><i class="uil uil-club"></i>Games</a></li>
+                  <li><a href="#" class="nav-link px-2"><i class="uil uil-game-structure"></i>Leaderboard</a></li>
+                  <li><a href="#" class="nav-link px-2"><i class="uil uil-schedule"></i>Reservation</a></li>
+                  <li><a href="#" class="nav-link px-2"><i class="uil uil-info-circle"></i>About Us</a></li>
                 </ul>
                 <div class="col-md-3 text-end btn-login-mobile-menu p-2">
-                <?php if (isset($_SESSION['voornaam'])){?>
-                <button type="button" class="btn btnLogin-mobile btn-dark me-2"><a href="./login.php" class="loginBtn-mobile text-decoration-none"><i class="uil uil-signout"></i>Logout</a></button>
-                <?php } else { ?>
-                <button type="button" class="btn btnLogin-mobile btn-dark me-2"><a href="./login.php" class="loginBtn-mobile text-decoration-none"><i class="uil uil-user"></i>Login</a></button>
-                <?php } ?>
+                  <button type="button" class="btn btnLogin-mobile btn-dark me-2"><a href="" class="loginBtn-mobile text-decoration-none"><i class="uil uil-user"></i>Login</a></button>
               </div>
             </div>
         </header>
         <main>
-          <section id="hero-img">
-            <img src="images/home.webp" alt="photo">
+          <section id="hero">
+            <div class="img">
+              <img width="300px" src="images/home/brownie-with-ai-generated-free-png 1.png" alt="hero - image">
+            </div>
+            <h2>Welcome To --</h2> 
+            <h1>Brawnies </h1>
+            <h1>Escape Rooms</h1>
           </section>
-          <section id="header">
-            <h1>Welcome To Brawnies Escape Room</h1>
-            <p>Experience the thrill of solving puzzles and escaping room challenging rooms. </p>
+          <section id="about">
+            <p>Experience the thrill of solving puzzles and escaping rom challenging rooms.</p>
           </section>
-          
-          <section id="online-games">
-            <h2>Online Games</h2>
-            <div class="row">
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Game 1</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Learn more</a>
+          <section id="onlineGames">
+            <div class="game">
+              <div class="imageHolder">
+                <img src="images//home/the-lost-key-background 1.png" alt="online game 1 image">
+              </div>
+              <div class="description">
+                <span>/online Games</span>
+                <hr>
+                <h2>01 "The Lost Key"</h2>
+                <hr>
+                <p>You find yourself in a mysterious office belonging to a renowned puzzle enthusiast, Mr. Enigma. Known for his love of brainteasers, </p>
+                <hr>
               </div>
             </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Game 2</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Learn more</a>
+            <div class="game">
+              <div class="imageHolder">
+                <img src="images//home/the-lost-key-background 1.png" alt="online game 1 image">
+              </div>
+              <div class="description">
+                <span>/online Games</span>
+                <hr>
+                <h2>02 "Het Verloren Tijdmachine"</h2>
+                <hr>
+                <p>You find yourself in a mysterious office belonging to a renowned puzzle enthusiast, Mr. Enigma. Known for his love of brainteasers, </p>
+                <hr>
               </div>
             </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Game 3</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Learn more</a>
+            <div class="game">
+              <div class="imageHolder">
+                <img src="images//home/the-lost-key-background 1.png" alt="online game 1 image">
+              </div>
+              <div class="description">
+                <span>/online Games</span>
+                <hr>
+                <h2>03 "Wally's Hamburger"</h2>
+                <hr>
+                <p>You find yourself in a mysterious office belonging to a renowned puzzle enthusiast, Mr. Enigma. Known for his love of brainteasers, </p>
+                <hr>
               </div>
             </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Game 4</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <span class="non-clikable-buttons" style="background-color: #6c757d;">Coming Soon!!</span>
-              </div>
-            </div>
-
-          </div>
           </section>
-          <section id="offline-games">
+          <section id="liverooms">
             <h2>Live Rooms</h2>
-            <div class="row">
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Room 1</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Learn more</a>
+            <!-- Flicker -->
+            <div class="carousel" id="carousel" data-flickity='{"autoPlay":true}'>
+              <div class="carousel-cell" style="background-image: url(images/home/lawrence-chismorie-o2ZIh9Bqjtc-unsplash\ 1.png);">
+              </div>
+              <div class="carousel-cell" style="background-image: url(images/home/peter-herrmann-bhF8gu9DaU4-unsplash\ 1.png);">
+              </div>
+              <div class="carousel-cell" style="background-image: url(images/home/marina-reich-qC1JcYEOOv8-unsplash\ 1.png);">
               </div>
             </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Room 2</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Learn more</a>
-              </div>
-            </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Room 3</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Learn more</a>
-              </div>
-            </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Room 4</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <span class="non-clikable-buttons" style="background-color: #6c757d;">Coming Soon!!</span>
-              </div>
-            </div>
-
-            <div class="card" style="width: 22rem;">
-              <img src="./images/home/jaredd-craig-HH4WBGNyltc-unsplash 1.webp" class="card-img-top" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Room 5</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <span class="non-clikable-buttons" style="background-color: #6c757d;">Coming Soon!!</span>
-              </div>
-            </div>
-
-          </div>
+            
+            <p>Explore our captivating rooms and secure your reservation for an enhanced escape room experience. Whether you're seeking the thrill of live adventures or the convenience of online challenges, we've got you covered!</p>
+            <a class="learnMore" href="./pages/games.php">Learn More</a>
           </section>
-
         </main>
-        <footer>
+      <footer>
         <div class="row">
           <div class="logo-footer col-6 col-md-2 mb-3">
           <img src="images/logo.webp">
@@ -214,5 +176,7 @@
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"
     ></script>
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+ 
   </body>
 </html>
